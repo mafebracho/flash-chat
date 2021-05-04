8246a177dc511c693942132066a5986509e42008
Mafe Bracho <mafe.bracho@gmail.com>
2021-05-04 15:37:11 +0200

    first commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by María Fernanda Bracho Salazar on 24.08.20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
